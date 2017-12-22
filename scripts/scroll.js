8f8e9b7c18f161daad5b06da13e1d73e450bbcbd
Amy Tu <tu.amy.a@gmail.com>
2017-12-21 20:52:25 -0800

    Added favicon, fixed scroll for classwork tabs

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
   $("a").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
+    if (this.hash !== "" && this.hash !=="#have-taken" && this.hash !=="#currently-taking" && this.hash !== "#will-take") {
       // Prevent default anchor click behavior
       event.preventDefault();
 
