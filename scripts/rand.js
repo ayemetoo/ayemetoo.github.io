b8118662f6dd39f36278c055cbdb1ffd85884340
Amy Tu <tu.amy.a@gmail.com>
2017-12-21 20:04:57 -0800

    Added scrolling and interactivity

---
+++ scripts/rand.js
@@ -0,0 +1,8 @@
+//Change title if page is not active
+window.onblur = function() {
+  document.title = ':(';
+}
+
+window.onfocus = function() {
+  document.title = 'Amy Tu';
+}
--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,5 +1,6 @@
 window.onscroll = function() {scrollFunction()};
 
+//scroll for navbar
 function scrollFunction() {
   console.log(document.body.scrollTop + ', ' + document.documentElement.scrollTop);
   if ((document.body.scrollTop > 500) || (document.documentElement.scrollTop > 500)) {
@@ -16,3 +17,29 @@ function scrollFunction() {
     document.getElementById("navbar").style.opacity = "0.5";
   }
 }
+
+//smooth scrolling between sections --> from W3C
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
