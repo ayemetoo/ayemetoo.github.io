a818a5dc52c8d193bcbd2aa2553cdcfeb5dedd46
Amy Tu <tu.amy.a@gmail.com>
2017-12-22 13:37:17 -0800

    Added contact information

--- scripts/rand.js
+++ scripts/rand.js
@@ -1,8 +1,28 @@
 //Change title if page is not active
 window.onblur = function() {
-  document.title = ':(';
+  document.title = ':( Come Back...';
 }
 
 window.onfocus = function() {
   document.title = 'Amy Tu';
+}
+
+//...Does something
+var codePresses = ['ArrowUp','ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+var codeIndex = 0;
+
+window.onkeydown = function(evt) {
+  var code = document.querySelector('#code-entered');
+  if (evt.key === codePresses[codeIndex]) {
+    codeIndex++;
+  } else {
+    codeIndex = 0;
+  }
+  console.log(codeIndex);
+
+  if (codeIndex === 10) {
+    alert("You are a nerd!");
+    code.style.opacity = 1;
+    code.style.width = 100 + "%";
+  }
 }
